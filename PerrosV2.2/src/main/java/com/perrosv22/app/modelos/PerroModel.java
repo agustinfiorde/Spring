cc027aadf5e8045411aa18bb971fc3f015ffe0f2
Agustin Fiordelisi <agulirio@yahoo.com.ar>
2021-01-19 13:52:09 -0300

    Refactor Login con redes sociales

--- PerrosV2.2/src/main/java/com/perrosv22/app/modelos/PerroModel.java
+++ PerrosV2.2/src/main/java/com/perrosv22/app/modelos/PerroModel.java
@@ -11,14 +11,14 @@ import lombok.Data;
 public class PerroModel implements Serializable {
 
 	private static final long serialVersionUID = 6522896498689132123L;
-	
+
 	private String id;
 
 	private UsuarioModel usuario;
 	private String idUsuario;
-	
+
 	private String foto;
-	
+
 	private String nombre;
 	private String apodo;
 	private String raza;
---
+++ PerrosV4.3/src/main/java/com/quinto/knowledge/social/SecurityConfig.java
@@ -0,0 +1,22 @@
+package com.quinto.knowledge.social;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+	    http.authorizeRequests()
+	      .antMatchers("/oauth_login", "/")
+	      .permitAll()
+	      .anyRequest()
+	      .authenticated()
+	      .and()
+	      .oauth2Login()
+	      .loginPage("/oauth_login")
+	      .defaultSuccessUrl("/loginSuccess");
+	}
+}
